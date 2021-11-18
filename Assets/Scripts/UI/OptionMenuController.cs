b7805c43c2e41f67a9787017286e72dd79f03bbe
Tom Leyrie <Tom.leyrie@gmail.com>
2021-11-18 17:24:19 -0500

    Menu rework

---
+++ Assets/Scripts/UI/OptionMenuController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenuController : MonoBehaviour
+{
+    private TabChanger CurrentTab;
+
+    [SerializeField]private List<TabChanger> tabs = new List<TabChanger>();
+
+    private void Start()
+    {
+        CurrentTab = tabs[0];
+        CurrentTab.StartCoroutine(CurrentTab.LeftTranslation());
+        CurrentTab.SetColor(Color.green);
+    }
+
+    public void ChangeTab(TabChanger Tab)
+    {
+        if (CurrentTab != Tab)
+        {
+            CurrentTab.StartCoroutine(CurrentTab.RightTranslation());
+            CurrentTab.SetColor(Color.white);
+        }
+
+        CurrentTab = Tab;
+        CurrentTab.SetColor(Color.green);
+    }
+
+    public TabChanger GetCurrentTab()
+    {
+        return CurrentTab;
+    }
+}
--- Assets/Scripts/UI/TabChanger.cs
+++ Assets/Scripts/UI/TabChanger.cs
@@ -8,16 +8,22 @@ using UnityEngine.UI;
 public class TabChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler
 {
     private Button Tab;
+    private Image ButtonImage;
     private Vector3 StartingPos;
     
     private Coroutine CurrentRoutine;
     private const float AnimTime = 0.2f;
     private const float Translation = 40f;
 
-    private void Start()
+    private OptionMenuController OptionController;
+
+    private void Awake()
     {
         Tab = GetComponent<Button>();
+        ButtonImage = GetComponent<Image>();
         StartingPos = Tab.transform.position;
+
+        OptionController = GetComponentInParent<OptionMenuController>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -32,21 +38,30 @@ public class TabChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (CurrentRoutine != null)
+        if (OptionController.GetCurrentTab() != this)
         {
-            StopCoroutine(CurrentRoutine);
-        }
+            if (CurrentRoutine != null)
+            {
+                StopCoroutine(CurrentRoutine);
+            }
         
-        CurrentRoutine = StartCoroutine(RightTranslation());
+            CurrentRoutine = StartCoroutine(RightTranslation());
+        }
+
+    }
+
+    public void SetColor(Color color)
+    {
+        ButtonImage.color = color;
     }
 
     public void OnSelect(BaseEventData eventData)
     {
-        
+
     }
 
 
-    private IEnumerator LeftTranslation()
+    public IEnumerator LeftTranslation()
     {
         float timer = 0f;
         Vector3 curPos = Tab.transform.position;
@@ -63,7 +78,7 @@ public class TabChanger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         }   
     }
     
-    private IEnumerator RightTranslation()
+    public IEnumerator RightTranslation()
     {
         float timer = 0f;
         Vector3 curPos = Tab.transform.position;
